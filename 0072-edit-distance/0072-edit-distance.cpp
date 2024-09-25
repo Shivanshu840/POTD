7528e65ee5570a59e083bc258e8fa53fb4eaeef6
Shivanshu raj <rajshivanshu.544@gmail.com>
2024-09-25 12:37:11 +0530

    Time: 3 ms (93.35%), Space: 12.9 MB (41.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,24 +2,45 @@ class Solution {
 public:
 
     vector<vector<int>>dp;
-    int solve(int m, int n, string s1, string s2){
-        if(m==0 || n==0) return m+n;
 
-        if(dp[m][n]!=-1) return dp[m][n];
+    // it is done by the memoization method
 
-        if(s1[m-1]==s2[n-1]) return dp[m][n] = solve(m-1,n-1,s1,s2);
 
-        int insert = 1 + solve(m,n-1,s1,s2);
-        int del = 1 + solve(m-1,n,s1,s2);
-        int rep = 1 + solve(m-1,n-1,s1,s2);
+    // int solve(int m, int n, string s1, string s2){
+    //     if(m==0 || n==0) return m+n;
 
-        return dp[m][n] = min({insert,del,rep});
-    }
+    //     if(dp[m][n]!=-1) return dp[m][n];
+
+    //     if(s1[m-1]==s2[n-1]) return dp[m][n] = solve(m-1,n-1,s1,s2);
+
+    //     int insert = 1 + solve(m,n-1,s1,s2);
+    //     int del = 1 + solve(m-1,n,s1,s2);
+    //     int rep = 1 + solve(m-1,n-1,s1,s2);
+
+    //     return dp[m][n] = min({insert,del,rep});
+    // }
     int minDistance(string word1, string word2) {
         
         int len1 = word1.size();
         int len2 = word2.size();
-        dp.resize(len1+1,vector<int>(len2+1,-1)); 
-        return solve(len1,len2,word1,word2);
+        dp.resize(len1+1,vector<int>(len2+1));
+        // return solve(len1,len2,word1,word2); 
+
+        for(int i = 0;i<=len1;i++){
+            for(int j=0;j<=len2;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = i + j;
+                }
+                else if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                }
+
+            
+            }
+        }
+       return dp[len1][len2];
     }
 };
