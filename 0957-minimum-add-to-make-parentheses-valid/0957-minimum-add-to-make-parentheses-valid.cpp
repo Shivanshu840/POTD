07634f1c9c5698cfa961e24ccf3b29c7ad8abee8
Shivanshu raj <rajshivanshu.544@gmail.com>
2024-10-09 09:03:08 +0530

    Time: 0 ms (100%), Space: 8 MB (32.94%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(!st.empty()){
+                if(st.top()=='(' && s[i]==')'){
+                    st.pop();
+                }else{
+                    st.push(s[i]);
+                }
+            }else{
+                st.push(s[i]);
+            }
+        }
+        return st.size();
+
+        
+    }
+};
