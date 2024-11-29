b7dfa49ba2c1b7810ef283355e57ac9533a55dcc
Shivanshu raj <rajshivanshu.544@gmail.com>
2024-11-29 18:37:22 +0530

    Time: 110 ms (7.93%), Space: 76 MB (5.03%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solution(string &s,int i, int j){
-        if(i>=j){
-        if(i==j) return 1;
-         return 0;
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s[i]==s[j]){
-
-            return dp[i][j] =  2 + solution(s,i+1,j-1);
-        }else{
-
-            return dp[i][j] =  max(solution(s,i+1,j),solution(s,i,j-1));
-        }
-
-    }
     int longestPalindromeSubseq(string s) {
 
-        int len = s.size();
-        dp.resize(len+1,vector<int>(len+1,-1));
+        // this is 2nd approach reverse the orignal string and then apply the lcs pattern here--> this will be able to solve the palindromic subsequence problem
+        string text1 = s;
+        reverse(s.begin(),s.end());
+        string text2 = s;
+        int len1 = text1.size();
+        int len2 = text2.size();
 
+        dp.resize(len1+1,vector<int>(len2+1));
 
-        return solution(s,0,len-1);
+        for(int i = 0;i<=len1;i++){
+
+            for(int j = 0; j<=len2; j++){
+
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i<=len1;i++){
+
+            for(int j = 1; j<=len2; j++){
+
+                if(text1[i-1]==text2[j-1]){
+
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
         
     }
 };
