aa6a3d1970dc1820bb539d093267236f066135fd
Shivanshu raj <rajshivanshu.544@gmail.com>
2024-12-11 11:15:26 +0530

    Time: 0 ms (100%), Space: 16.7 MB (37.38%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -2,48 +2,54 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
 
-        vector<vector<int>> ans = grid;
-        queue<pair<pair<int,int>,int>>q;
-        for(int i =0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]==2){
-                    pair<int,int>cordi = {i,j};
-                    q.push({cordi,0});
+        vector<vector<int>>& grid2 = grid;
+
+        queue<pair<pair<int, int>, int>> q;
+
+        for (int i = 0; i < grid.size(); i++) {
+
+            for (int j = 0; j < grid[i].size(); j++) {
+
+                if (grid2[i][j] == 2) {
+                    q.push({{i, j}, 0});
                 }
             }
         }
         int anstime = 0;
-        while(!q.empty()){
-            auto front = q.front();
+        while (!q.empty()) {
+
+            auto frontNode = q.front();
+            pair<int, int> dir = frontNode.first;
+            int time = frontNode.second;
+            int x = dir.first;
+            int y = dir.second;
             q.pop();
-            int x = front.first.first;
-            int y = front.first.second;
-            int time = front.second;
 
-            int dx[] = {-1,0,1,0};
-            int dy[] = {0,1,0,-1};
+            int dx[] = {-1, 0, 1, 0};
+            int dy[] = {0, 1, 0, -1};
 
-            for(int i=0;i<4;i++){
-                int newx = x + dx[i];
-                int newy = y + dy[i];
+            for (int i = 0; i < 4; i++) {
 
-                if(newx>=0 && newx<ans.size() && newy>=0 && newy<ans[0].size()&& ans[newx][newy]==1){                
-                    anstime = max(anstime,time+1);
-                    pair<int,int> cordinate = {newx,newy};
-                    q.push({cordinate,time+1});
-                    ans[newx][newy] = 2;
-                }
-            }
-            
-        }
-        for(int i =0;i<ans.size();i++){
-            for(int j=0;j<ans[i].size();j++){
-                if(ans[i][j]==1){
-                    return -1;
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if (newX >= 0 && newX < grid2.size() && newY >= 0 &&
+                    newY < grid[0].size() && grid2[newX][newY] == 1) {
+
+                    anstime = max(anstime, time + 1);
+                    q.push({{newX, newY}, time + 1});
+                    grid2[newX][newY] = 2;
                 }
             }
         }
+
+        for(int i =0;i<grid2.size();i++){
+            for(int j = 0; j<grid2[i].size();j++){
+
+                if(grid2[i][j]==1) return -1;
+            }
+        }
+
         return anstime;
-        
     }
 };
