2e13bf1627ff4f0cf95e00265b85a1084c8e5bef
Shivanshu raj <rajshivanshu.544@gmail.com>
2024-12-30 18:57:44 +0530

    Time: 2 ms (17.79%), Space: 9.5 MB (5.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,8 +14,28 @@ public:
         return dp[i][j] =  up + down;
     }
     int uniquePaths(int m, int n) {
-        dp.resize(m+1,vector<int>(n+1,-1));
-        return solve(m-1,n-1);
+        dp.resize(m+1,vector<int>(n+1,0));
+        // return solve(m-1,n-1);
+
+        for(int i = 0;i<m;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int j = 0;j<n;j++){
+            dp[0][j] = 1;
+        }
+
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+
+                int up = dp[i-1][j];
+                int down = dp[i][j-1];
+
+                dp[i][j] = up + down;
+            }
+        }
+
+        return dp[m-1][n-1];
         
     }
 };
