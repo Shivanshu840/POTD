dc68671c7491cd6bba6f7d848c32e9590a1c9d8f
Shivanshu raj <rajshivanshu.544@gmail.com>
2025-03-05 18:41:14 +0530

    Time: 0 ms (100%), Space: 14 MB (94.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-// top-down approach
-    vector<vector<int>>dp;
-    int solve(vector<int>&nums, int prev, int index){
-
-        if(index>=nums.size()) return 0;
-        if(dp[index][prev+1]!=-1) return dp[index][prev+1];
-        int take = 0;
-        if(prev == -1 || nums[index]>nums[prev]){
-            take = 1 + solve(nums,index,index+1);
-        }
-        int notTake = 0 + solve(nums,prev,index+1);
-
-        return dp[index][prev+1] = max(take,notTake);
-    }
-
-
     int lengthOfLIS(vector<int>& nums) {
+        
+        vector<int>lis;
 
-        int prev = -1;
-        int index = 0;
-        int n = nums.size();
-        // dp.resize(n+1,vector<int>(n+1,0));
-        // return solve(nums,prev,index);
+        for(int i = 0;i<nums.size();i++){
 
-        // bottom-up-approach
+            if(lis.empty() || lis.back()<nums[i]){
 
-        dp.resize(n+1,vector<int>(n+1,0));
+                lis.push_back(nums[i]);
+            }else{
 
-
-        for(int i = n-1;i>=0;i--){
-
-            for(int prev = i-1; prev>=-1;prev--){
-                int take = 0;
-                if(prev==-1 || nums[i]>nums[prev]){
-
-                    take = 1 + dp[i+1][i+1];
-                }
-                int notTake = 0 + dp[i+1][prev+1];
-
-                dp[i][prev+1] = max(take,notTake);
-                
+                auto it = lower_bound(lis.begin(),lis.end(),nums[i]);
+                *it = nums[i];
             }
         }
 
-        return dp[0][0];
-
-
-        
+        return lis.size();
     }
 };
